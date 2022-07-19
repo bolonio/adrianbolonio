dfb83215b2252bc14fecbfe8be8832c437e90e93
Adrián Bolonio <bolonio@github.com>
2022-07-19 19:03:27 +0200

    Add link to blog in tag page

--- pages/blog/tag/[tag].tsx
+++ pages/blog/tag/[tag].tsx
@@ -1,5 +1,7 @@
 import { BlogPostContainer } from "@components/BlogPostContainer"
+import Box from "@components/core/Box"
 import { PageHeading } from "@components/core/Headings"
+import Text from "@components/core/Text"
 import { PageLayoutContent } from "@components/layouts/Layout"
 import { SEO } from "@components/Seo"
 import { languages } from "@data/languages"
@@ -7,8 +9,9 @@ import introImage from "@images/blog.jpg"
 import { BlogPost, getBlogPostsByTag, getTags } from "@lib/blog"
 import type { GetStaticProps } from "next"
 import Image from "next/image"
+import Link from "next/link"
 import { useRouter } from "next/router"
-import { useIntl } from "react-intl"
+import { FormattedMessage, useIntl } from "react-intl"
 
 const Tag = ({ posts, tag }: { posts: BlogPost[]; tag: string }) => {
   const { locale } = useRouter()
@@ -26,9 +29,24 @@ const Tag = ({ posts, tag }: { posts: BlogPost[]; tag: string }) => {
         alt={intl.formatMessage({ id: "blog_page_image_alt" })}
       />
       <PageLayoutContent>
-        <PageHeading level={1}>
-          {`${intl.formatMessage({ id: "title_tag" })}: ${tag}`}
-        </PageHeading>
+        <Box
+          display="flex"
+          alignItems="baseline"
+          flexDirection={["column", "row", "row"]}
+          justifyContent="space-between"
+          marginBottom={["16px", 0, 0]}
+        >
+          <PageHeading level={2}>
+            {`${intl.formatMessage({ id: "title_tag" })}: ${tag}`}
+          </PageHeading>
+          <Link href="/blog" passHref>
+            <a>
+              <Text fontSize="1.5rem" fontWeight={700}>
+                <FormattedMessage id="all_posts" />
+              </Text>
+            </a>
+          </Link>
+        </Box>
         <BlogPostContainer posts={posts} />
       </PageLayoutContent>
     </section>
